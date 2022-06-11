7a27755e3bd36ac18641a539eda39688666d2891
Rahmi Azizah <79822759+rahmi-na@users.noreply.github.com>
2022-06-11 23:43:04 +0700

    add alt in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,51 @@
-import React, { useState } from 'react';
-import Logo from '../assets/Logo.png';
+import React, { useState } from "react";
+import Logo from "../assets/Logo.png";
 import { Link } from "react-router-dom";
-import '../style/Navbar.css';
-import ReorderIcon from '@mui/icons-material/Reorder';
+import "../style/Navbar.css";
+import ReorderIcon from "@mui/icons-material/Reorder";
 
 function Navbar() {
-    // Variabel dimana akan berubah untuk re-render website dan akan menampilkan perubahan yang dibuat, seperti 'if'
-    const [openLinks, setOpenLinks] = useState(false)
+  // Variabel dimana akan berubah untuk re-render website dan akan menampilkan perubahan yang dibuat, seperti 'if'
+  const [openLinks, setOpenLinks] = useState(false);
 
-    const toggleNavbar = () => {
-        // Membuat fungsi dimana akan mengubah id value menjadi sebaliknya
-        setOpenLinks(!openLinks);
-    }
+  const toggleNavbar = () => {
+    // Membuat fungsi dimana akan mengubah id value menjadi sebaliknya
+    setOpenLinks(!openLinks);
+  };
 
   return (
-    <div className='navbar'>
-        <div className='leftSide' id={openLinks ? "open" : "close"}>
-            {/* Logo Di Sebelah Kiri Atas */}
-            <img src={Logo} />
-            {/* Link Responsive Ketika Website Seukuran Handphone */}
-            <div className='hiddenLinks'>
-            <Link to="/"> Beranda </Link>
-            <Link to="/menu"> Menu </Link>
-            <Link to="/about"> About </Link>
-            <Link to="/customerService"> CS </Link>
-            <Link to="/kalkulatorPajak" className='NavbarButton'> Kalkulator </Link>
-            </div>
+    <div className="navbar">
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
+        {/* Logo Di Sebelah Kiri Atas */}
+        <img src={Logo} alt="" />
+        {/* Link Responsive Ketika Website Seukuran Handphone */}
+        <div className="hiddenLinks">
+          <Link to="/"> Beranda </Link>
+          <Link to="/menu"> Menu </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/customerService"> CS </Link>
+          <Link to="/kalkulatorPajak" className="NavbarButton">
+            {" "}
+            Kalkulator{" "}
+          </Link>
         </div>
-        {/* Link Button */}
-        <div className='rightSide'>
-            <Link to="/"> Beranda </Link>
-            <Link to="/menu"> Menu </Link>
-            <Link to="/about"> Tentang Kami </Link>
-            <Link to="/customerService"> Customer Service </Link>
-            <Link to="/kalkulatorPajak" className='NavbarButton'> Kalkulator Pajak </Link>
+      </div>
+      {/* Link Button */}
+      <div className="rightSide">
+        <Link to="/"> Beranda </Link>
+        <Link to="/menu"> Menu </Link>
+        <Link to="/about"> Tentang Kami </Link>
+        <Link to="/customerService"> Customer Service </Link>
+        <Link to="/kalkulatorPajak" className="NavbarButton">
+          {" "}
+          Kalkulator Pajak{" "}
+        </Link>
         <button onClick={toggleNavbar}>
-            <ReorderIcon/>
+          <ReorderIcon />
         </button>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
